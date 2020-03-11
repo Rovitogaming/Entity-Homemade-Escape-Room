886667a9f92d861bc1d8eb44483bc73d5e62cf9c
Rovitogaming <52048853+Rovitogaming@users.noreply.github.com>
2020-03-11 19:18:46 -0400

    Add files via upload

---
+++ ControlPanelWifi.ino
@@ -0,0 +1,90 @@
+#include <ESP8266WiFi.h>
+#include<PubSubClient.h>
+
+int ReadState = 1;
+bool DoorOpen = false;
+
+void callback(char* topic,byte* payload,unsigned int length1){    
+Serial.print("message arrived[");
+Serial.print(topic);
+Serial.println("]");
+
+for(int i=0;i<length1;i++){
+  Serial.print(payload[i]); 
+}
+
+
+if(payload[0]==49) {  //1 OFF
+digitalWrite(4, LOW);
+}
+else if(payload[0]==50){ //2 ON
+digitalWrite(4, HIGH);
+}
+Serial.println();
+}
+
+void setup() {
+  delay( 1000 ); // power-up safety delay
+  Serial.begin(115200);
+  Serial.print("connecting");
+  pinMode(2, INPUT);
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+  WiFi.begin("TAZMANHOME","F68ACRogers");         //SSID,PASSWORD 
+  while(WiFi.status()!=WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  
+  reconnect();
+
+}
+
+
+
+const char* mqtt_server="192.168.86.27";
+WiFiClient espclient;
+PubSubClient client(mqtt_server,1883,callback,espclient);
+
+void reconnect(){
+  while(WiFi.status()!=WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  while(!client.connected()){
+  if(client.connect("controlpanelclient")){
+    Serial.println("connected");
+    client.subscribe("controlIn");
+  }
+    else{
+      Serial.print("failed,rc=");
+      Serial.println(client.state());
+      delay(500);
+    }
+  } 
+}
+
+
+
+void loop() {
+    if(!client.connected()){
+      reconnect();
+    }
+    ReadState = digitalRead(2);
+    if (ReadState == LOW) {
+      if (DoorOpen == false) {
+    client.publish("controlOut","2");
+    DoorOpen = true;
+    Serial.println("Recieved Control Signal");
+      }
+    }
+    else{
+      if (DoorOpen == true) {
+        client.publish("controlOut","1");
+        DoorOpen = false;
+      }
+    }
+    client.loop();
+    
+    }
